952a07b2d3dbc66acb50aa703c57de6b60c48482
Replit user <replituser@example.com>
2021-03-26 19:20:55 +0000

    new try

--- list.c
+++ list.c
@@ -29,7 +29,10 @@ Node * createNode(const void * data) {
 }
 
 List * createList() {
-     return NULL;
+
+  List* lista = (List*) malloc(sizeof(List));
+
+     return lista;
 }
 
 void * firstList(List * list) {
